7310e8ff548c29e2d6556c88de4035d4e7041173
Dair Smith <dair.code@gmail.com>
2021-04-12 15:49:22 -0400

    using proper documents for test

--- script.js
+++
@@ -1,9 +0,0 @@
-const isDead = (health) => {
-    if (health <= 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-//open spec and run html in browser to show tests
--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
 "use strict";
 
 // TODO - write your code here.
+
+const randomDamage = () => {
+    return Math.floor(math.random() * 10) + 1;
+}
+
+const chooseOption = (opt1, opt2) => {
+    let randNum = Math.round(Math.random());
+    return (randNum === 0) ? "True" : "False";
+}
+
+const isDead = (health) => {
+    if (health <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//open spec and run html in browser to show tests
